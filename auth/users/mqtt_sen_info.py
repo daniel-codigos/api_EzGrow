1c1de9e13c8a7e7ca27205520b27108352ea2ac9
- <xatak12@gmail.com>
2025-01-05 21:19:38 +0100

    V1

---
+++ auth/users/mqtt_sen_info.py
@@ -0,0 +1,89 @@
+import pymongo
+import time
+import paho.mqtt.client as mqtt
+import json
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+mqtt_user = os.getenv("mqtt_user")
+mqtt_passwd = os.getenv("mqtt_passwd")
+mqtt_ip = os.getenv("mqtt_ip")
+MONGO_URI = "mongodb://localhost:27017/"
+MONGO_DB = "mqtt_data"
+MONGO_COLLECTION = "mqtt_messages"
+
+class get_info():
+    def __init__(self, cualTopic,token):
+        self.mqtt_broker = mqtt_ip
+        self.mqtt_user = mqtt_user
+        self.mqtt_pass = mqtt_passwd
+        self.mqtt_topic = cualTopic
+        self.senToken = token
+        self.respuesta = False
+        self.fin = None
+        self.mqtt_client = self.login_mqtt()
+
+    def on_message(self, client, userdata, msg):
+        message = msg.payload.decode("utf-8")
+        if self.is_valid_json(message):
+            recibido = json.loads(message)
+            if 'token' in recibido and recibido['token'] == self.senToken:
+                print(recibido)
+                self.fin = recibido
+                self.respuesta = True
+
+    def is_valid_json(self, json_str):
+        try:
+            json.loads(json_str)
+            return True
+        except json.JSONDecodeError:
+            return False
+
+    def save_to_mongodb(self, topic, payload):
+        client = pymongo.MongoClient(MONGO_URI)
+        db = client[MONGO_DB]
+        collection = db[MONGO_COLLECTION]
+
+        document = {
+            "topic": topic,
+            "payload": payload
+        }
+
+        collection.insert_one(document)
+        client.close()
+
+    def login_mqtt(self):
+        client = mqtt.Client()
+        client.on_message = self.on_message
+        client.username_pw_set(self.mqtt_user, password=self.mqtt_pass)
+        client.connect(self.mqtt_broker, 1883, 60)
+        client.subscribe(self.mqtt_topic)
+        return client
+
+    def take_info(self, timeout=55):
+        try:
+            self.mqtt_client.loop_start()
+            self.mqtt_client.publish(self.mqtt_topic, "send " + self.senToken)
+
+            start_time = time.time()
+            while not self.respuesta and (time.time() - start_time) < timeout:
+                time.sleep(1)
+
+            if not self.respuesta:
+                print("Timeout alcanzado, no se recibió respuesta.")
+                return None
+
+        except KeyboardInterrupt:
+            print("\nSaliendo del programa.")
+        finally:
+            self.mqtt_client.loop_stop()
+            self.mqtt_client.disconnect()
+
+        return self.fin
+
+# Ejemplo de uso
+#if __name__ == "__main__":
+    #sensor_info = get_info("sen_water_dist", "EfV6jrp5dedffMV")
+    #data = sensor_info.take_info()
+    #print(data)
